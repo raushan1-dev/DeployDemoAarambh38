0f67baeb4024562192fcbe5150d5ab624ac3270d
Raushan Raj <kumarraushanraj10@gmail.com>
2025-07-27 18:13:50 +0530

    error

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Vercel expects this as the default, just being explicit
+  }
 })
